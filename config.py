c7320d35f6fd2929fdb468b83b0be14d7557ec61
pdd0911 <dinhduytth1us@gmail.com>
2026-06-19 08:36:48 +0900

    commit dev container

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import os
+
+
 class ConfigDB:
-    HOSTNAME = "localhost"
-    DATABASE = "RetrievalSystemTraffic"
-    USERNAME = "postgres"
-    # PASSWORD = "123456"
-    PASSWORD = "09111997"
-    PORT = 5432 #5432 for Local #5433 for csMachine
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
+    HOSTNAME = os.environ.get("POSTGRES_HOST", "localhost")
+    DATABASE = os.environ.get("POSTGRES_DB", "RetrievalSystemTraffic")
+    USERNAME = os.environ.get("POSTGRES_USER", "postgres")
+    PASSWORD = os.environ.get("POSTGRES_PASSWORD", "09111997")
+    PORT = int(os.environ.get("POSTGRES_PORT", "5432"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"postgresql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}",
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
